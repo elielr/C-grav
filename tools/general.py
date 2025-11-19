c90d3db38bddab257dd6a89e5e310cc356e4b26e
elielr <elgit@stiva.fr>
2025-11-19 05:33:53 +0100

    restructuration (tools/general, tools/gridsearch and save/planets_config), new color scheme (and dark pyplot), short explanation in Introduction.ipynb

---
+++ tools/general.py
@@ -0,0 +1,132 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+plt.style.use('dark_background')    
+
+class Planets :
+    def __init__(self, N, bounds, radius, G):
+        self.N = N # number of planets
+        self.bounds = bounds # boundaries of the planet generation
+        self.span = np.array([self.bounds[0,1]-self.bounds[0,0], self.bounds[1,1]-self.bounds[1,0]])
+        self.radius = radius #radius of planets
+        self.G = G # gravity constant (multiplied by planet mass)
+        self.loc = np.zeros((N,2)) # planets locations
+
+    def gen_planets(self,bounds,centralisation=0,trymax=10):
+        ntry = 0
+        self.loc = np.zeros((self.N,2))
+        while np.min(4*self.radius*np.eye(self.N)+np.linalg.norm(self.loc[None,:,:]-self.loc[:,None,:], axis = 2))<=3*self.radius and ntry<trymax :
+            self.loc = (1-centralisation) * self.span * (np.random.random(size=(self.N,2))-0.5) + np.mean(bounds,axis=1)
+            ntry+=1
+        if np.min(4*self.radius*np.eye(self.N)+np.linalg.norm(self.loc[None,:,:]-self.loc[:,None,:], axis = 2)) <= 3*self.radius :
+            print("Failed to generate {} NON-colliding planets in {} tries, should reduce their number, radius or enlarge the frame bounds".format(self.N, trymax))
+        return self.loc        
+
+    def A(self, pos):
+        a = np.zeros(2)
+        for i in range(self.N):
+            d_i = self.loc[i] - pos
+            a += d_i/np.linalg.norm(d_i, axis=-1)**3
+        return self.G*a
+
+    def potential(self, pos):
+        pot = 0
+        for i in range(self.N):
+            d_i = self.loc[i]-pos[...,:]
+            pot -= self.G/np.linalg.norm(d_i, axis=-1)
+        return pot
+
+class Trajectory(Planets) :
+    def __init__(self, N, radius, G, bounds, dt, Tmax):
+        Planets.__init__(self, N, bounds, radius, G)
+        self.p0 = np.zeros(2) # initial position
+        self.p = np.zeros(2) # shuttle current position
+        self.v = np.zeros(2) # shuttle current speed
+        self.dt = dt # sampling time
+        self.Tmax = Tmax # maximum trajectory simulation time, if reached trajectory is considered infinite
+        self.p_hist = [] # successive positions of the trajectory
+        self.v_hist = [] # successive speeds
+
+    def gen_shuttle(self, centralisation=0, trymax=10):
+        self.p0 = self.loc[0,:]
+        ntry=0
+        while np.min(np.linalg.norm(self.loc-self.p0[None,:], axis = 1))<=2*self.radius and ntry<trymax :
+            self.p0 = (1-centralisation)*self.span*(np.random.random(size=(2))-0.5)+np.mean(self.bounds,axis=1)
+            ntry+=1
+        return self.p0
+        
+    def leapfrog(self):
+        #https://en.wikipedia.org/wiki/Leapfrog_integration
+        v12 = self.v + 0.5 * self.dt * self.A(self.p)
+        pos1 = self.p + self.dt * v12
+        v1 = v12 + 0.5 * self.dt * self.A(pos1)
+        return pos1, v1
+
+    def stop_cond(self, border=True):
+        for i in range(self.N):
+            if np.linalg.norm(self.p-self.loc[i,:])<self.radius:
+                return i+1 # crashed in planet i
+        if border and (self.p[0]<self.bounds[0,0] or self.p[0]>self.bounds[0,1] or self.p[1]<self.bounds[1,0] or self.p[1]>self.bounds[1,1]):
+            return self.N + 1 # crashed on border
+        if len(self.p_hist) * self.dt >= self.Tmax:
+            return self.N + 2 # reached maximum time limit
+        
+    def compute_traj(self, border=True):
+        while not self.stop_cond(border):
+            self.p_hist.append(self.p)
+            self.v_hist.append(self.v)
+            self.p, self.v = self.leapfrog()
+        self.p_hist.append(self.p)
+        self.v_hist.append(self.v)
+        return np.array(self.p_hist), np.array(self.v_hist)
+
+    def reset(self,p0=None,v0=np.zeros(2),loc=None):
+        if not p0 is None :
+            self.p0 = p0
+        if not loc is None :
+            self.loc = loc
+            self.N = self.loc.shape[0]
+        self.p_hist, self.v_hist = [], []
+        self.p, self.v = self.p0, v0
+
+class Display:
+    def __init__(self, traj, n=1000, color_list=['orchid', 'mediumorchid', 'darkorchid', 'blueviolet', 'rebeccapurple', 'purple', 'darkmagenta','mediumvioletred','hotpink']):
+        self.n = n
+        self.traj = traj
+
+        self.x, self.y = np.linspace(self.traj.bounds[0,0], self.traj.bounds[0,1], self.n), np.linspace(self.traj.bounds[1,0], self.traj.bounds[1, 1], self.n)
+        self.xx, self.yy = np.meshgrid(self.x,self.y)
+        self.xy = np.stack([np.repeat(self.x,self.y.size).reshape((self.x.size,self.y.size)),np.tile(self.y,self.x.size).reshape((self.x.size,self.y.size))], axis=-1)
+        
+        self.pot = self.traj.potential(self.xy)
+        self.pot_min = -self.traj.N*self.traj.G/self.traj.radius # minimum potential for colorbars, approximately potential value at planet surface
+
+        self.colormap = colors.ListedColormap(color_list[:self.traj.N]+["#081838", "#8FE8FF"])
+        self.colorbar_ticklabels = ['planet {}'.format(i+1) for i in range(self.traj.N)] + ['border', '(infinite)']
+
+    def plot_config(self,title=False,ax=None):
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(4,4),layout='constrained',subplot_kw = {'aspect':1})
+        if title:
+            ax.set_title('Initial configuration : p0 = [{}, {}]'.format(self.traj.p0[0],self.traj.p0[1]))
+        c = ax.pcolormesh(self.xx, self.yy, self.pot.T, cmap='binary',vmin=self.pot_min,vmax=0)
+        ax.axis([self.traj.bounds[0,0], self.traj.bounds[0,1], self.traj.bounds[1,0], self.traj.bounds[1,1]])
+        ax.set_xticks([]),ax.set_yticks([])
+        for i in range(self.traj.N):
+            ax.add_patch(plt.Circle((self.traj.loc[i,0] , self.traj.loc[i,1]),self.traj.radius,color='k'))
+        ax.scatter(self.traj.p0[0],self.traj.p0[1],c='gold',marker='*',s=100)
+
+    def plot_traj(self,v0,ax,border=True):
+        self.traj.reset(v0=v0)
+        P,_ = self.traj.compute_traj(border)
+        ax.set_title('v = [{:.2f}, {:.2f}] : T = {:.2f}'.format(v0[0],v0[1],(P.shape[0]-1)*self.traj.dt))
+        ax.pcolormesh(self.xx, self.yy, self.pot.T, cmap='binary',vmin=self.pot_min,vmax=0)
+        ax.axis([self.traj.bounds[0,0], self.traj.bounds[0,1], self.traj.bounds[1,0], self.traj.bounds[1,1]])
+        ax.set_aspect((self.y.max()-self.y.min())/(self.x.max()-self.x.min()))
+        ax.set_xticks([]),ax.set_yticks([])
+        for i in range(self.traj.N):
+            ax.add_patch(plt.Circle((self.traj.loc[i,0] , self.traj.loc[i,1]),self.traj.radius,color='k'))
+        ax.scatter(self.traj.p0[0],self.traj.p0[1],c='gold',marker='x',s=40,zorder=3)
+        ax.arrow(self.traj.p0[0],self.traj.p0[1],v0[0],v0[1],color='r',head_width=0.05,alpha=0.8,length_includes_head=True,zorder=2)
+        ax.plot(P[:,0],P[:,1],linewidth=1,zorder=1)
---
+++ tools/gridsearch.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tools.general import Display
+
+class GridSearch :
+    def __init__(self,traj,n,bounds=None):
+        self.traj = traj # instance of Trajectory
+        self.ngrid = n #grid resolution
+        self.bounds = bounds # boundaries of solution exploration
+        if bounds is None:
+            self.bounds = self.traj.bounds
+        
+        self.gridx, self.gridy = np.linspace(self.bounds[0,0], self.bounds[0,1], self.ngrid), np.linspace(self.bounds[1,0], self.bounds[1, 1], self.ngrid)
+        self.gridxx, self.gridyy = np.meshgrid(self.gridx,self.gridy)
+        self.gridxy = np.stack([np.repeat(self.gridx,self.gridy.size).reshape((self.gridx.size,self.gridy.size)),np.tile(self.gridy,self.gridx.size).reshape((self.gridx.size,self.gridy.size))],axis=-1)
+        
+        self.cond = np.zeros((self.gridx.size,self.gridy.size))
+        self. score = np.zeros((self.gridx.size,self.gridy.size))
+
+        self.opt_ind = np.zeros((2,1))
+        self.opt = np.zeros(2)
+        self.vbest = np.zeros(2)
+        
+    def compute_grid(self, border=True):
+        for i in range(self.gridx.size):
+            for j in range(self.gridy.size):
+                self.traj.reset(v0=np.array([self.gridx[i],self.gridy[j]])-self.traj.p0)
+                P, _ = self.traj.compute_traj(border)
+                self.cond[i,j] = self.traj.stop_cond(border)
+                self.score[i,j] = (P.shape[0]-1)*self.traj.dt
+
+    def vbest_grid(self):
+        self.opt_indices = np.array(np.where(self.score == self.score.max())).T
+        self.opt = self.opt_indices[np.random.randint(self.opt_indices.shape[0])]
+        self.vbest = self.gridxy[self.opt[0],self.opt[1]] - self.traj.p0
+        print('{} maximum(s) - proposal [{}, {}] : v = [{:.2f},{:.2f}]'.format(self.opt_indices.shape[0],self.opt[0],self.opt[1],self.vbest[0],self.vbest[1]))
+        self.traj.reset(v0=self.vbest)
+
+class DisplayGridSearch(Display, GridSearch):
+    def __init__(self, traj, n, bounds = None):
+        Display.__init__(self, traj)
+        GridSearch.__init__(self, traj, n, bounds)
+        self.compute_grid()
+        self.vbest_grid()
+
+    def heatmap_crashsite(self, ax, fig):
+        ax.set_title('Final destination')
+        ms = ax.pcolormesh(self.gridxx, self.gridyy, self.cond.T, cmap = self.colormap, vmin=0.5, vmax=self.traj.N+2.5)
+        ax.axis([self.gridxx.min(), self.gridxx.max(), self.gridyy.min(), self.gridyy.max()])
+        ax.set_xticks([]),ax.set_yticks([])
+        cbar = fig.colorbar(ms,ticks = np.arange(self.traj.N+2)+1)
+        cbar.ax.set_yticklabels(self.colorbar_ticklabels,rotation='vertical',verticalalignment='center')
+        for i in range(self.traj.N):
+            ax.add_patch(plt.Circle((self.traj.loc[i,0], self.traj.loc[i,1]), self.traj.radius,color='k',alpha=0.2))
+            ax.text(self.traj.loc[i,0] , self.traj.loc[i,1],'{}'.format(i+1),horizontalalignment='center',verticalalignment='center',c='w',alpha=0.5,size=20)
+        ax.scatter((self.traj.p0+self.vbest)[0],(self.traj.p0+self.vbest)[1], s=80, facecolors='none', edgecolors='r',linewidths=2)
+        ax.scatter(self.traj.p0[0],self.traj.p0[1],c='gold',marker='+',s=100)        
+
+    def heatmap_score(self, ax, fig):
+        ax.set_title('Flight time')
+        pcm2 = ax.pcolormesh(self.gridxx, self.gridyy, self.score.T,cmap='managua', norm='log')
+        ax.scatter((self.traj.p0+self.vbest)[0],(self.traj.p0+self.vbest)[1], s=80, facecolors='none', edgecolors='r',linewidths=2)
+        ax.axis([self.gridxx.min(), self.gridxx.max(), self.gridyy.min(), self.gridyy.max()])
+        ax.set_xticks([]),ax.set_yticks([])
+        if np.max(self.score.T)==self.traj.Tmax:
+            cbar = fig.colorbar(pcm2,extend='max')
+
+        else :
+            cbar = fig.colorbar(pcm2)
+        cbar.set_ticks(ticks=[10**i for i in range(int(np.floor(np.log10(np.max(self.score))+1)))]); cbar.ax.yaxis.set_minor_formatter(plt.NullFormatter())
+
+    def plot4(self, figsize = (16,16), save = False):
+        fig, axs = plt.subplots(2, 2, figsize=figsize,layout='constrained',subplot_kw = {'aspect':1})
+        self.plot_config(title=True, ax=axs[0,0])
+        self.plot_traj(self.vbest,axs[1,0])
+        self.heatmap_crashsite(axs[0,1],fig)
+        self.heatmap_score(axs[1,1],fig)
+        if save :
+            np.save('save/planets_loc/[{}, {}].npy'.format(self.traj.p0[0],self.traj.p0[1]),self.traj.loc)
+            fig.savefig('figs/[{}, {}] - {} (g{}).png'.format(self.traj.p0[0],self.traj.p0[1],self.traj.N,self.n),bbox_inches='tight')
