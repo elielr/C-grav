fca4f6d10560a1712aadf5b4e31001fbe1e6ea06
elielr <elgit@stiva.fr>
2025-12-03 00:58:43 +0100

    line multisearch

--- tools/linesearch.py
+++ tools/linesearch.py
@@ -14,7 +14,7 @@ class LineSearch:
     def __init__(self, traj, n=100, v1=None, v2=None):
         self.n = n # total number of evaluations omitting those at v1 and v2
         self.traj = traj
-        self.eval = np.zeros((n+2,3),dtype='float')
+        self.eval = np.zeros((n+2,3),dtype='float') # stores the linear abscisse, the destination and the score associated
         self.V = np.zeros((n+2,2))
 
         self.v0 = np.zeros((2,2))
@@ -27,14 +27,14 @@ class LineSearch:
             self.eval[-1,1:] = self.ending(self.v0[1])
         else :
             self.eval[-1,1] = self.eval[0, 1]
-            while self.eval[-1,1] == self.eval[0,1]:
+            while self.eval[-1,1] == self.eval[0,1]: # we want v0 and v1 to have different destinations to look at the frontiers on the line
                 self.v0[1] = self.traj.span*(np.random.random(size=(2))-0.5)+np.mean(self.traj.bounds,axis=1) - self.traj.p0
                 self.eval[-1,1:] = self.ending(self.v0[1])
         
         self.opt_ind = np.zeros((2,1))
         self.opt = np.zeros(2)
         self.vbest = np.zeros(2)
-        
+
     def ending(self, v, border=True):
         self.traj.reset(v0=v)
         P, _ = self.traj.compute_traj(border)
@@ -43,12 +43,13 @@ class LineSearch:
     #v = lambda self, a : (1-a)*self.v0[0] + a*self.v0[1]
     v = lambda self, a : np.array([(1-a)*self.v0[0,0]+a*self.v0[1,0], (1-a)*self.v0[0,1]+a*self.v0[1,1]]).T # moins classe mais efficace
 
+    evaluate = lambda self, a : np.concatenate([[a],self.ending(self.v(a))]) # np.concatenate([A[:,None],self.endings(self.v(A))]) if we get a parallel version of ending
+
     ### The Methods ###
     
-    # https://en.wikipedia.org/wiki/Line_search doesn't apply here, even 0-order golden-section, our function is absolutely not unimodal
+    # https://en.wikipedia.org/wiki/Line_search doesn't apply here, even 0-order golden-section, our function is absolutely not unimodal, and random local maxima will be disappointing
     
     def uniform_search(self):
-        self.eval[1:-1] = np.zeros((self.n,3))
         self.eval[1:-1,0] = np.linspace(0,1,self.n+2)[1:-1]
         for i in range(1,self.n+1):
             self.eval[i,1:] = self.ending(self.v(self.eval[i,0]))
@@ -59,9 +60,8 @@ class LineSearch:
         # v1 and v2 are in zones of different destinations (i.e. trajectory crash region), we zoom in on a (any) frontier between two detinations
         a0, a1 = 0, self.n+1
         self.eval[1:-1] = np.zeros((self.n,3))
-        for i in range(1,min(self.n+1,50)): # 0.5**50 = 1e-15 no need to go much below double precision since position takes values around 1
-            self.eval[i,0] = (self.eval[a0,0]+self.eval[a1,0])/2
-            self.eval[i,1:] = self.ending(self.v(self.eval[i,0]))
+        for i in range(1,min(self.n+1,50)): # 0.5**50 = 1e-15 no need to go too close to double precision since position takes values close to 1
+            self.eval[i] = self.evaluate((self.eval[a0,0]+self.eval[a1,0])/2)
             if self.eval[i,1] == self.eval[a0,1]:
                 a0 = i
             else :
@@ -72,7 +72,7 @@ class LineSearch:
     def mixed_search(self,n_step=100):
         # if n_step == 1 it is the dicho_frontiersearch, if n_step == self.n it is the uniform_search, inbetween it's a balance
         n_zoom = min(self.n//n_step, -int(np.emath.logn(n_step+1, 0.5**50))) # no need to zoom ad eternam due to float double precision
-        self.eval[1:-1] = np.zeros((self.n,3))        
+        self.eval[1:-1] = np.zeros((self.n,3))      
         a0, a1 = 0, self.n+1
         for i in range(n_zoom):
             self.eval[i*n_step+1:(i+1)*n_step+1,0] = np.linspace(self.eval[a0,0],self.eval[a1,0],n_step+2)[1:-1]
@@ -84,7 +84,7 @@ class LineSearch:
             mask *= np.maximum(self.eval[:,2],np.roll(self.eval[:,2],-1))
             mask[a0] = (self.eval[a0,1] != self.eval[i*n_step+1,1]) * np.max([self.eval[a0,2], self.eval[i*n_step+1,2]])
             mask[(i+1)*n_step] = (self.eval[a1,1] != self.eval[(i+1)*n_step,1]) * np.max([self.eval[a1,2], self.eval[(i+1)*n_step,2]])
-            # select zommed in region of interest
+            # select zoommed in region of interest
             a2 = np.argmax(mask)
             if a2 == a0:
                 a1 = i*n_step+1
@@ -94,12 +94,49 @@ class LineSearch:
                 a0, a1 = a2, a2+1
         self.V = self.v(self.eval[:,0])
         return self.eval.copy()
+    
+    def dicho_multisearch(self):
+        # since we only go up to 0.5**50 we can zoom in on multiple frontiers if the three crashes are different a0!=a2!=a1
+        self.eval[1:-1] = np.zeros((self.n,3))
+        L = [(0,self.n+1)]
+        n_call = 0
+        while n_call<self.n and len(L)>0:
+            a0, a1 = L.pop()
+            if (self.eval[a1,0]-self.eval[a0,0])>0.5**50:
+                n_call+=1
+                mid = (self.eval[a0,0]+self.eval[a1,0])/2
+                self.eval[n_call,:] = self.evaluate(mid)
+                if self.eval[n_call,1] != self.eval[a1,1]:
+                    L.append((n_call,a1))
+                if self.eval[n_call,1] != self.eval[a0,1]:
+                    L.append((a0,n_call)) # left first to match dicho_frontiersearch
+        self.V = self.v(self.eval[:,0])
+        return self.eval.copy()
 
-    # dicho_multisearch, if a!=b!=c explore on both sides
-
-    # mixed_multidir_search, reinitialize v0[1] multiple times to look for wider optimums in easier zones
-
-    # mixed_multidir_multisearch
+    def mixed_mutlisearch(self,n_step=100):
+        self.eval[1:-1] = np.zeros((self.n,3))
+        L = [(0,self.n+1,0.)]
+        n_call = 0
+        while n_call<self.n and len(L)>0:
+            a0, a1, _ = L.pop() # L.pop(0) for a width first search but we want to explore around one maximum, not get a general idea of the evolution 
+            if (self.eval[a1,0]-self.eval[a0,0])>0.5**50: # we don't want to get too close to double precision
+                self.eval[n_call+1:n_call+n_step+1,0] = np.linspace(self.eval[a0,0],self.eval[a1,0],n_step+2)[1:-1]
+                # left border case
+                self.eval[n_call+1] = self.evaluate(self.eval[n_call+1,0])
+                L_new = [(a0,n_call+1,max(self.eval[a0,2],self.eval[n_call+1,2]))] if self.eval[a0,1]!=self.eval[n_call+1,1] else []
+                # inside loop, compare to left neighbor
+                for j in range(2,n_step+1):
+                    self.eval[n_call+j] = self.evaluate(self.eval[n_call+j,0])
+                    if self.eval[n_call+j,1]!=self.eval[n_call+j-1,1]:
+                        L_new.append((n_call+j-1,n_call+j,max(self.eval[n_call+j-1,2],self.eval[n_call+j,2])))
+                # right border case
+                if self.eval[n_call+n_step,1]!=self.eval[a1,1]:
+                    L_new.append((n_call+n_step,a1,max(self.eval[n_call+n_step,2],self.eval[a1,2])))
+                L_new.sort(key=lambda x:(x[-1],-x[0])) # sorting the new candidates, the best scores will be on top, in case of tie take the segment of smallest abscisse (so it matches mixed_search)
+                L += L_new # putting new candidates list on top of full list, no full sorting we explore around one maximum
+                n_call+=n_step
+        self.V = self.v(self.eval[:,0])
+        return self.eval.copy()
 
     ### Optimum ###
     
@@ -110,6 +147,7 @@ class LineSearch:
         if verbose:
             print('{} maximum(s) - proposal [{}] : v = [{:.2f},{:.2f}]'.format(self.opt_indices.shape[0],self.opt,self.vbest[0],self.vbest[1]))
         self.traj.reset(v0=self.vbest)
+
 
 
 class DisplayLineSearch(Display, LineSearch):
@@ -129,8 +167,8 @@ class DisplayLineSearch(Display, LineSearch):
         ymax = np.min([self.traj.Tmax, 10**int(np.floor(np.log10(ymax))) * (1+int(np.floor(ymax/10**int(np.floor(np.log10(ymax))))))])
         if ax is None:
             fig, ax = plt.subplots(figsize=(16,3),layout='constrained')
-        ax.vlines(self.eval[:,0][self.eval[:,1] == self.traj.N+2], ymin, ymax, colors="#8FE8FF",zorder=1)
-        ax.vlines(self.eval[np.argmax(self.eval[:,2]),0], ymin, self.eval[np.argmax(self.eval[:,2]),2], colors='r', zorder=2)
+        ax.vlines(self.eval[:,0][self.eval[:,1] == self.traj.N+2], 0, ymax, colors="#8FE8FF",zorder=1)
+        ax.vlines(self.eval[np.argmax(self.eval[:,2]),0], 0, self.eval[np.argmax(self.eval[:,2]),2], colors='r', zorder=2)
         ms = ax.scatter(self.eval[:,0][(self.eval[:,2]>0) * (self.eval[:,1] != self.traj.N+2)],self.eval[:,2][(self.eval[:,2]>0) * (self.eval[:,1] != self.traj.N+2)],
                         c=self.eval[:,1][(self.eval[:,2]>0) * (self.eval[:,1] != self.traj.N+2)],cmap=self.colormap,s=2,vmin=0.5,vmax=self.traj.N+2.5,zorder=3)
         if self.eval[np.argmax(self.eval[:,2]),1] != self.traj.N+2:
@@ -197,3 +235,5 @@ class DisplayLineSearch(Display, LineSearch):
             np.save('save/planets_loc/[{}, {}].npy'.format(self.traj.p0[0],self.traj.p0[1]),self.traj.loc)
             np.save('save/line/[{}, {}].npy'.format(self.traj.p0[0],self.traj.p0[1]),self.v0)
             fig.savefig('figs/[{}, {}] - 2D (l{}).png'.format(self.traj.p0[0],self.traj.p0[1],self.traj.N,self.n),bbox_inches='tight')
+
+# Future idea: multidir_search, reinitialize v0[1] multiple times to look for wider optimums in easier zones, should be it's own class, n/n_dir calls in ech direction
