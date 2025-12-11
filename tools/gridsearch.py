748abbaeef8283189a4cb2f0951d95140e3dcc9d
elielr <elgit@stiva.fr>
2025-12-11 08:28:57 +0100

    gridsearch fig.colorbar to plt.colorbar

--- tools/gridsearch.py
+++ tools/gridsearch.py
@@ -40,29 +40,29 @@ class DisplayGridSearch(Display, GridSearch):
         self.compute_grid()
         self.vbest_grid()
 
-    def heatmap_crashsite(self, ax, fig):
+    def heatmap_crashsite(self, ax):
         ax.set_title('Final destination')
         pcm = ax.pcolormesh(self.gridxx, self.gridyy, self.res[...,0].T, cmap = self.colormap, vmin=0.5, vmax=self.traj.N+2.5)
         ax.axis([self.gridxx.min(), self.gridxx.max(), self.gridyy.min(), self.gridyy.max()])
         ax.set_xticks([]),ax.set_yticks([])
-        cbar = fig.colorbar(pcm,ticks = np.arange(self.traj.N+2)+1)
+        cbar = plt.colorbar(pcm,ticks = np.arange(self.traj.N+2)+1)
         cbar.ax.set_yticklabels(self.colorbar_ticklabels,rotation='vertical',verticalalignment='center')
         for i in range(self.traj.N):
             ax.add_patch(plt.Circle((self.traj.loc[i,0], self.traj.loc[i,1]), self.traj.radius,color='k',alpha=0.2))
             ax.text(self.traj.loc[i,0] , self.traj.loc[i,1],'{}'.format(i+1),horizontalalignment='center',verticalalignment='center',c='w',alpha=0.5,size=20)
         ax.scatter((self.traj.p0+self.vbest)[0],(self.traj.p0+self.vbest)[1], s=80, facecolors='none', edgecolors='r',linewidths=2)
-        ax.scatter(self.traj.p0[0],self.traj.p0[1],c='gold',marker='+',s=100)        
+        ax.scatter(self.traj.p0[0],self.traj.p0[1],c='gold',marker='+',s=100)
 
-    def heatmap_score(self, ax, fig):
+    def heatmap_score(self, ax):
         ax.set_title('Flight time')
         pcm2 = ax.pcolormesh(self.gridxx, self.gridyy, self.res[...,1].T,cmap='managua', norm='log')
         ax.scatter((self.traj.p0+self.vbest)[0],(self.traj.p0+self.vbest)[1], s=80, facecolors='none', edgecolors='r',linewidths=2)
         ax.axis([self.gridxx.min(), self.gridxx.max(), self.gridyy.min(), self.gridyy.max()])
         ax.set_xticks([]),ax.set_yticks([])
         if np.max(self.res[...,1].T)==self.traj.Tmax:
-            cbar = fig.colorbar(pcm2,extend='max')
+            cbar = plt.colorbar(pcm2,extend='max')
         else :
-            cbar = fig.colorbar(pcm2)
+            cbar = plt.colorbar(pcm2)
         cbar.set_ticks(ticks=[10**i for i in range(int(np.floor(np.log10(np.max(self.res[...,1]))+1)))]); cbar.ax.yaxis.set_minor_formatter(plt.NullFormatter())
 
     def plot4(self, figsize = (16,16), title = None, save = False):
@@ -71,8 +71,8 @@ class DisplayGridSearch(Display, GridSearch):
             fig.suptitle(title)
         self.plot_config(title=True, ax=axs[0,0])
         self.plot_traj(self.vbest,axs[1,0])
-        self.heatmap_crashsite(axs[0,1],fig)
-        self.heatmap_score(axs[1,1],fig)
+        self.heatmap_crashsite(axs[0,1])
+        self.heatmap_score(axs[1,1])
         if save :
             np.save('save/planets_loc/[{}, {}].npy'.format(self.traj.p0[0],self.traj.p0[1]),self.traj.loc)
             title = '[{}, {}] - g({})'.format(self.traj.p0[0],self.traj.p0[1],self.ngrid) if title is None else title
